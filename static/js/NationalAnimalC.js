7626f3ff4e60ed4534789c230da544b9773ac92c
m6 <m6@gmail.com>
2018-01-18 04:04:39 -0800

    Initial commit of Demo6 workspace

---
+++ static/js/NationalAnimalC.js
@@ -0,0 +1,27 @@
+/* globals D6App1 */
+
+(function() {
+
+  D6App1.NationalAnimalC = Trillo.inherits(Trillo.Controller, function(viewSpec) {
+    Trillo.Controller.call(this, viewSpec);
+  });
+
+  var NationalAnimalC = D6App1.NationalAnimalC.prototype;
+  var Controller = Trillo.Controller.prototype;
+
+  NationalAnimalC.handleAction = function(actionName, selectedObj, $e, targetController) {
+    if (actionName === "edit") {
+      this.showForm(selectedObj);
+      return true;
+    }
+    return Controller.handleAction.call(this, actionName, selectedObj);
+  };
+  
+  NationalAnimalC.showForm = function showForm(selectedObj) {
+    this.showView({
+      name : "CountryForm",
+      container : 'trillo-dialog2-container',
+      data : selectedObj
+    });
+  };
+})();
